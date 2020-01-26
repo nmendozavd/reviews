aa91065bd5e20b854e2ecb9008059b3c7235cffb
Gal Weinstock <gal.weinstock@post.idc.ac.il>
2020-01-25 20:17:13 -0800

    Work on the endpoint to get reviews

--- server/index.js
+++ server/index.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 2020
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 2020;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hi there, this is the main endpoint\n'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get(
+  '/v1/api/:accommodationId/reviews',
+  (req, res) => {
+    mongoose.connect('mongodb://localhost/airbnbReviews', { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
+      console.log("we're connected to airbnb DB!");
+    })
+
+    res.send(`Hi, you've reached /v1/api/${req.params.accommodationId}/reviews endpoint\n`);
+    // Get all reviews for a specific house sorted by date
+    db.Reviews.find({"accommodationId" : req.params.accommodationId}).sort({"reviewAuthorDetails.date": -1}).limit(7).pretty()
+
+    // Route path: /users/:userId/books/:bookId
+    // Request URL: http://localhost:3000/users/34/books/8989
+    // req.params: { "userId": "34", "bookId": "8989" }
+  }
+)
+
+app.listen(port, () => console.log(`Reviews server is listening on port ${port}!\n`))
