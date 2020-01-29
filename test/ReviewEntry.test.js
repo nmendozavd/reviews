b894329e7322a66dde2ea96f32a62fe8e95d78b8
Gal Weinstock <gal.weinstock@post.idc.ac.il>
2020-01-29 15:36:05 -0800

    Clean the comments

--- test/ReviewEntry.test.js
+++ test/ReviewEntry.test.js
@@ -15,8 +15,6 @@ describe(
       reviewText: 'lorem ipsum'
     };
 
-    ;
-
     it('Displays the avatar of the user', () => {
       const wrapper = shallow(<ReviewEntry review={review} />)
       expect(wrapper.find('img').props().src).toEqual('portrait1.jpg');
@@ -24,19 +22,16 @@ describe(
 
     it('Displays the first name of the user', () => {
       const wrapper = shallow(<ReviewEntry review={review} />);
-      // expect(wrapper.find('h2').text()).toEqual('David');
       expect(wrapper.contains('David')).toBe(true);
     });
     
     it('Displays the date of the review', () => {
       const wrapper = shallow(<ReviewEntry review={review} />);
-      // expect(wrapper.find('h2').text()).toEqual('January 2020');
       expect(wrapper.contains('January 2020')).toBe(true);
     });
     
     it('Displays the text of the review', () => {
       const wrapper = shallow(<ReviewEntry review={review} />);
-      // expect(wrapper.find('h2').text()).toEqual('lorem ipsum');
       expect(wrapper.contains('lorem ipsum')).toBe(true);
     });
   }
