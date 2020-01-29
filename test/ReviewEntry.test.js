eb74067f11b8ea4b3344aa2755f52dd1e2e6d427
Gal Weinstock <gal.weinstock@post.idc.ac.il>
2020-01-29 13:47:52 -0800

    Implement test for one review entry

--- test/ReviewEntry.test.js
+++ test/ReviewEntry.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import ReviewEntry from '../client/src/components/ReviewEntry.jsx'
+import ReviewEntry from '../client/src/components/ReviewEntry.jsx';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -15,24 +15,29 @@ describe(
       reviewText: 'lorem ipsum'
     };
 
+    ;
+
     it('Displays the avatar of the user', () => {
-      const wrapper = shallow(<ReviewEntry review={review} />);
+      const wrapper = shallow(<ReviewEntry review={review} />)
       expect(wrapper.find('img').props().src).toEqual('portrait1.jpg');
     });
 
     it('Displays the first name of the user', () => {
       const wrapper = shallow(<ReviewEntry review={review} />);
-      expect(wrapper.find('h2').text()).toEqual('David');
+      // expect(wrapper.find('h2').text()).toEqual('David');
+      expect(wrapper.contains('David')).toBe(true);
     });
-
+    
     it('Displays the date of the review', () => {
       const wrapper = shallow(<ReviewEntry review={review} />);
-      expect(wrapper.find('h2').text()).toEqual('January 2020');
+      // expect(wrapper.find('h2').text()).toEqual('January 2020');
+      expect(wrapper.contains('January 2020')).toBe(true);
     });
-
+    
     it('Displays the text of the review', () => {
       const wrapper = shallow(<ReviewEntry review={review} />);
-      expect(wrapper.find('h2').text()).toEqual('lorem ipsum');
+      // expect(wrapper.find('h2').text()).toEqual('lorem ipsum');
+      expect(wrapper.contains('lorem ipsum')).toBe(true);
     });
   }
 );
