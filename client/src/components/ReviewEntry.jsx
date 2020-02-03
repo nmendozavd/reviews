e9a3e76cf19bbb500e41e764e04a6c7ca4d08838
Gal Weinstock <gal.weinstock@post.idc.ac.il>
2020-02-03 12:23:23 -0800

    Make the avatar, name, date and text look like the real component

--- client/src/components/ReviewEntry.jsx
+++ client/src/components/ReviewEntry.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import styles from '../../dist/styles/styles.css';
 
+const months = ['zero', 'January', 'February',
+  'March', 'April', 'May',
+  'June', 'July', 'August',
+  'September', 'October', 'November', 'December'];
+
 const ReviewEntry = ({ review }) => {
-  console.log(review);
+  // Creating nicely looking date:
+  // From somthing like that: 2020-01-26T05:35:14.858Z
+  // To something like that: January 2020
+  const date = review.reviewAuthorDetails.date.split('-')[0] + ' ' +
+  months[parseInt(review.reviewAuthorDetails.date.split('-')[1])];
+
   return (
     <div className={styles.reviewEntry}>
       <div className={styles.reviewHeader}>
         <div className={styles.avatarContainer}>
-          <img src={review.reviewAuthorDetails.userPicture} alt="avatar"/>
+          <img src={review.reviewAuthorDetails.userPicture} alt="avatar" />
         </div>
         <div className={styles.authoringDetails}>
-          <div className={styles.firstName}>{review.reviewAuthorDetails.name}</div>
-          <div className={styles.date}>{review.reviewAuthorDetails.date}</div>
+          <div className={styles.firstName}>{review.reviewAuthorDetails.name.split(' ')[0]}</div>
+          <div className={styles.date}>{date}</div>
         </div>
       </div>
       <div className={styles.reviewText}>{review.reviewAuthorDetails.reviewText}</div>
