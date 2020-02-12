70a8be82b7b00ed756c0596ead93daba94303cc1
Noel Mendoza <nmendozavd@gmail.com>
2020-02-12 12:12:19 -0800

    Added POST operation

--- server/index.js
+++ server/index.js
@@ -6,16 +6,35 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get(
-  '/v1/api/:accommodationId/reviews',
-  (req, res) => {
-    dbReviewModel.getAllReviewsForSpecificHouse(
-      (reviews) => {
-        res.send(reviews)
-      },
-      req.params.accommodationId
-    );
-  }
-)
+//CRUD Operations
+
+app.get('/v1/api/:accommodationId/reviews', (req, res) => {
+  dbReviewModel.getAllReviewsForSpecificHouse((reviews) => {
+    res.send(reviews);
+  },
+  req.params.accommodationId
+  );
+});
+
+app.post('/v1/api/1001/reviews', (req, res) => {
+  dbReviewModel.insertData ((reviews) => {
+    res.send('Successfully Posted to DB');
+    
+  },
+  // req.params.accommodationId
+  );
+});
+
+
+// app.put('/', (req, res) => {
+
+// });
+
+
+// app.delete('/', (req, res) => {
+
+// });
+
+
 
 app.listen(port, () => console.log(`Reviews server is listening on port ${port}!\n`))
