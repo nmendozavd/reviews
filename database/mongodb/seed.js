7a34043432ba167173b18b0f9e0afb5b4c1edc6f
Noel Mendoza <nmendozavd@gmail.com>
2020-02-19 15:40:06 -0800

    cleaned up code

--- database/mongodb/seed.js
+++ database/mongodb/seed.js
@@ -1,75 +1,93 @@
+const mongoose = require('mongoose');
 const faker = require('faker');
-const fs = require('fs');
-const { Parser } = require('json2csv');
+const Listing = require('./index.js')
+require('events').EventEmitter.prototype._maxListeners = 10000;
 
-
-let reviewsArr = [];
-
-for (let i = 0; i < 5; i++) {
-  reviewsArr.push({
-    review_id: i,
-    reviewerName: faker.name.findName(),
-    reviewerPhoto: `https://airbnb-reviews-users-pictures.s3-us-west-1.amazonaws.com/${Math.ceil(Math.random() * 3000)}.jpg`,
-    reviewerLink: faker.internet.url(),
-    date: faker.date.recent(),
-    reviewerComment: faker.lorem.paragraphs(),
-    scores: {
-      accuracy: faker.finance.amount(1, 5, 1),
-      communication: faker.finance.amount(1, 5, 1),
-      cleanliness: faker.finance.amount(1, 5, 1),
-      checkIn: faker.finance.amount(1, 5, 1),
-      value: faker.finance.amount(1, 5, 1),
-      location: faker.finance.amount(1, 5, 1),
-    },
-  })
+async function seedDB(outer, inner) {
+  let counter = 0;
+  for (let j = 0; j < outer; j++) {
+    let listingsArr = [];
+    for (let i = 0; i < inner; i++) {
+      let obj = {
+        listing_id: i,
+        ratingOverall: faker.finance.amount(1, 5, 1),
+        ratingCommunication: faker.finance.amount(1, 5, 1),
+        ratingCheckIn: faker.finance.amount(1, 5, 1),
+        ratingCleanliness: faker.finance.amount(1, 5, 1),
+        ratingAccuracy: faker.finance.amount(1, 5, 1),
+        ratingLocation: faker.finance.amount(1, 5, 1),
+        ratingValue: faker.finance.amount(1, 5, 1),
+        host: {
+          host_id: i,
+          hostName: faker.name.findName(),
+          hostPhoto: faker.internet.avatar(),
+        },
+        reviews: [
+          { 
+            review_id: i,
+            reviewerName: faker.name.findName(),
+            reviewerPhoto: faker.internet.avatar(),
+            reviewerLink: faker.internet.url(),
+            date: faker.date.recent(),
+            reviewerComment: faker.lorem.paragraphs(),
+            scores: {
+              accuracy: faker.finance.amount(1, 5, 1),
+              communication: faker.finance.amount(1, 5, 1),
+              cleanliness: faker.finance.amount(1, 5, 1),
+              checkIn: faker.finance.amount(1, 5, 1),
+              value: faker.finance.amount(1, 5, 1),
+              location: faker.finance.amount(1, 5, 1),
+            }
+          },
+          {
+            review_id: i+=1,
+            reviewerName: faker.name.findName(),
+            reviewerPhoto: faker.internet.avatar(),
+            reviewerLink: faker.internet.url(),
+            date: faker.date.recent(),
+            reviewerComment: faker.lorem.paragraphs(),
+            scores: {
+              accuracy: faker.finance.amount(1, 5, 1),
+              communication: faker.finance.amount(1, 5, 1),
+              cleanliness: faker.finance.amount(1, 5, 1),
+              checkIn: faker.finance.amount(1, 5, 1),
+              value: faker.finance.amount(1, 5, 1),
+              location: faker.finance.amount(1, 5, 1),
+            }
+          },
+          {
+            review_id: i+=2,
+            reviewerName: faker.name.findName(),
+            reviewerPhoto: faker.internet.avatar(),
+            reviewerLink: faker.internet.url(),
+            date: faker.date.recent(),
+            reviewerComment: faker.lorem.paragraphs(),
+            scores: {
+              accuracy: faker.finance.amount(1, 5, 1),
+              communication: faker.finance.amount(1, 5, 1),
+              cleanliness: faker.finance.amount(1, 5, 1),
+              checkIn: faker.finance.amount(1, 5, 1),
+              value: faker.finance.amount(1, 5, 1),
+              location: faker.finance.amount(1, 5, 1),
+            }
+          }
+        ]
+      };
+      listingsArr.push(obj);
+      counter++;
+      // console log progress
+      if (counter % 10000 === 0) {
+        console.log('progress', counter)
+      }
+    }
+    
+    await Listing.Listing.insertMany(listingsArr);
+  }
 }
 
-var listings = [];
 
-const data_size = 10000000;
-
-for (let j = 0; j < data_size; j++) {
-  listings.push(
-  {
-    listing_id: j,
-    ratingOverall: faker.finance.amount(1, 5, 1),
-    ratingCommunication: faker.finance.amount(1, 5, 1),
-    ratingCheckIn: faker.finance.amount(1, 5, 1),
-    ratingCleanliness: faker.finance.amount(1, 5, 1),
-    ratingAccuracy: faker.finance.amount(1, 5, 1),
-    ratingLocation: faker.finance.amount(1, 5, 1),
-    ratingValue: faker.finance.amount(1, 5, 1),
-    host: {
-      host_id: j,
-      hostName: faker.name.findName(),
-      hostPhoto: `https://airbnb-reviews-users-pictures.s3-us-west-1.amazonaws.com/${Math.ceil(Math.random() * 3000)}.jpg`
-    },
-    reviews: reviewsArr,
-  });
-};
-
-const fields = ['listing_id', 'ratingOverall', 'ratingCommunication', 'ratingCheckIn', 'ratingCleanliness', 'ratingAccuracy', 'ratingLocation', 'ratingValue', 'host.host_id', 'host.hostName', 'host.hostPhoto', 'reviews.review_id', 'reviews.reviewerName', 'reviews.reviewerPhoto', 'reviews.reviewerLink', 'reviews.date', 'reviews.reviewerComment', 'reviews.scores.accuracy', 'reviews.scores.communication', 'reviews.scores.cleanliness', 'reviews.scores.checkIn', 'reviews.scores.value', 'reviews.scores.location'];
-const json2csvParser = new Parser({ fields, unwind: ['reviews', 'reviews.scores'], unwindBlank: true });
-const csv = json2csvParser.parse(listings);
-
-
-fs.writeFile('mongo.csv', csv, function (err) {
-  if (err) throw err;
-  console.log('...done');
-});
-    
-
-//   // const listingEntry = new dbListingModel.Listing(listings);
-
-
-  // listingEntry.save((err, listing) => {
-  //   if(err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log(`Listing #${listingEntry.listing_id} has been saved to the DB!.`);
-  //   }
-  // })
-
-
-// };
+seedDB(10000, 1000);
+console.log('...done');
+console.time('seed time');
+console.timeEnd('seed time');
 
