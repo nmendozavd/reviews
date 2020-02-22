e4bc314fd2668907e5f5dc73b7701c11c43a7b9e
Noel Mendoza <nmendozavd@gmail.com>
2020-02-22 12:32:30 -0800

    added mysql connection

--- database/mysql/index.js
+++ database/mysql/index.js
@@ -1,11 +1,16 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
+  host: 'localhost',
   user: 'root',
   database: 'SDC'
 });
 
-db.connect();
-
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Ay dawg, connected to mysql!');
+});
 
 module.exports = db;
